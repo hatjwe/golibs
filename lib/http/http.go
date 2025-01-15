a1cc65f9e965fde4dc6296a533f215bb8baac78c
hatjwe <sunxin@moresec.cn>
2025-01-15 17:34:24 +0800

    新增http

--- lib/http/http.go
+++ lib/http/http.go
@@ -9,9 +9,10 @@ import (
 )
 
 type RequestInterface struct {
-	Url     string          `json:"url"`
-	Request HttpRespone     `json:"request"`
-	Body    *strings.Reader `json:"body"`
+	Url     string            `json:"url"`
+	Request HttpRespone       `json:"request"`
+	Body    *strings.Reader   `json:"body"`
+	Headers map[string]string `json:"headers"` // 存储请求头
 }
 
 // 设置跳过证书验证
@@ -46,6 +47,16 @@ func (s *RequestInterface) HttpGetRequest() HttpRespone {
 	return s.Https(Get, s.Url, strings.NewReader(""))
 
 }
+
+// 设置请求头
+func (s *RequestInterface) SetHeaders(headers map[string]string) {
+	if s.Headers == nil {
+		s.Headers = make(map[string]string)
+	}
+	for key, value := range headers {
+		s.Headers[key] = value
+	}
+}
 func (s *RequestInterface) Https(method, url string, body *strings.Reader) HttpRespone {
 	var Requset HttpRespone
 	request, err := http.NewRequest(method, url, body)
@@ -54,7 +65,10 @@ func (s *RequestInterface) Https(method, url string, body *strings.Reader) HttpR
 
 		return Requset
 	}
-
+	// 添加自定义请求头
+	for key, value := range s.Headers {
+		request.Header.Set(key, value)
+	}
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if response != nil {
